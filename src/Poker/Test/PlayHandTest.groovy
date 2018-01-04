2c3c99d038a2e2c4bc9360020a56df153fd92776
hnsz <hnsz>
2018-01-04 11:35:05 +0100

    Test was in wrong dir

---
+++ src/Poker/Test/PlayHandTest.groovy
@@ -0,0 +1,25 @@
+package Poker.Test
+
+import Poker.Player
+
+class PlayHandTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+        Player p1, p2, p3, p4;
+    }
+
+    void testStart() {
+    }
+
+    void testBettingRounds() {
+    }
+
+    void testShowdown() {
+    }
+
+    void testPayout() {
+    }
+
+    void testEnd() {
+    }
+}
